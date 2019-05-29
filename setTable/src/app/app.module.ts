71fd1ccaa63513f8382ebc08daef7a973a80a1dd
EtiVaizman <e6739995@gmail.com>
2019-05-29 14:47:13 +0300

    ok

--- setTable/src/app/app.module.ts
+++ setTable/src/app/app.module.ts
@@ -8,15 +8,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { NewServiceService } from './SHerd/new-service.service';
 import { LoginComponent } from './login/login.component';
 import { LogoComponent } from './logo/logo.component';
+import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
+const routes :Routes=[{
+  path:"Logo",
+  component:LogoComponent,
+
+}];
 
 @NgModule({
   declarations: [
     AppComponent,
     NewcompComponent,
     LoginComponent,
-    LogoComponent
-  
+    LogoComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
